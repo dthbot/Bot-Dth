b184608e7ab7a8c2d97713d72f1c0d8890cc8f6a
dthbot <blebledarius@gmail.com>
2026-01-22 02:39:44 +0100

    Update menu-gruppo.js

--- plugins/menu-gruppo.js
+++ plugins/menu-gruppo.js
@@ -109,7 +109,7 @@ const handler = async (message, { conn, usedPrefix }) => {
             { buttonId: `${usedPrefix}menuadmin`, buttonText: { displayText: '🛡️ Menu Admin' }, type: 1 },
             { buttonId: `${usedPrefix}menuowner`, buttonText: { displayText: '👑 Menu Owner' }, type: 1 },
             { buttonId: `${usedPrefix}menusicurezza`, buttonText: { displayText: '🚨 Menu Sicurezza' }, type: 1 },
-            { buttonId: `${usedPrefix}menuia`, buttonText: { displayText: '🤖 Menu IA' }, type: 1 }
+            { buttonId: `${usedPrefix}menumod`, buttonText: { displayText: '🤖 Menu MOD' }, type: 1 }
         ],
         viewOnce: true
     });
