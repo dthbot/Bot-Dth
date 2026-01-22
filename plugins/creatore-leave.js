e6c6bb6cb26c44176384d08a02390bb50dded99a
dthbot <blebledarius@gmail.com>
2026-01-22 17:24:46 +0100

    Update creatore-leave.js

--- plugins/creatore-leave.js
+++ plugins/creatore-leave.js
@@ -1,26 +1,7 @@
-let handler = async (m, { conn, text, command }) => {
-  const isOwner = [...global.owner.map(([number]) => number), ...global.mods].map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
-  if (!isOwner) {
-    if (!m.isAdmin) {
-      await m.reply('⚠️ Questo comando può essere usato solo da admin e owner del gruppo')
-      return
-    }
-  }
-
-  let id = text ? text : m.chat  
-  let chat = global.db.data.chats[m.chat]
-  
-  await conn.reply(id, `令 *varebot* sta abbandonando il gruppo, bella 👋`) 
-  await conn.groupLeave(id)
-  
-  try {
-  } catch (e) {
-    console.error('Errore durante l\'uscita:', e)
-    await m.reply('Si è verificato un errore durante l\'uscita dal gruppo')
-  }
-}
-
-handler.command = /^(esci|leavegc|leave|voltati)$/i
+let handler = async (m, { conn, args, command }) => {
+await m.reply('𝐦𝐢 𝐬𝐨𝐧𝐨 𝐫𝐨𝐭𝐭𝐨 𝐞𝐫 𝐜𝐚𝐳𝐳𝐨 𝐝𝐢 𝐬𝐭𝐨 𝐠𝐫𝐮𝐩𝐩𝐨, 𝐜𝐢𝐚𝐨') 
+await  conn.groupLeave(m.chat)}
+handler.command = /^(out|leavegc|leave|salirdelgrupo)$/i
 handler.group = true
-handler.owner = true
+handler.rowner = true
 export default handler
