6d715f5bc347f5e7a83461f0268d5ef44ca67aec
dthbot <blebledarius@gmail.com>
2026-01-25 10:44:32 +0100

    Update MOD-nukefaks.js

--- plugins/MOD-nukefaks.js
+++ plugins/MOD-nukefaks.js
@@ -1,23 +1,23 @@
-let handler = async (m, { conn, participants, isOwner, isAdmin, user }) => {
+let handler = async (m, { conn, participants, isOwner, user }) => {
 
-    // 🔐 CONTROLLO PERMESSI (MOD / PREMIUM)
-    if (!isOwner && !isAdmin && !user.premium) {
+    let isAdmin = participants
+        .find(p => p.id === m.sender)?.admin
+
+    let isPremium = user?.premium || false
+
+    if (!isOwner && !isAdmin && !isPremium) {
         return m.reply('⛔ *Questo comando è riservato ai MOD / PREMIUM*')
     }
 
-    // Prende in automatico il link del gruppo
     let code = await conn.groupInviteCode(m.chat)
     let link = `https://chat.whatsapp.com/${code}`
 
-    // Primo messaggio
     await conn.sendMessage(m.chat, { 
         text: "𝗤𝗨𝗘𝗦𝗧𝗢 𝗚𝗥𝗨𝗣𝗣𝗢 𝗘’ 𝗦𝗧𝗔𝗧𝗢 𝗗𝗢𝗠𝗜𝗡𝗔𝗧𝗢 𝗗𝗔 𝕯𝖊ⱥ𝖉𝖑𝐲 🔥" 
     })
 
-    // Menzioni invisibili
     let mentions = participants.map(u => u.id)
 
-    // Secondo messaggio con tag invisibili
     await conn.sendMessage(m.chat, { 
         text: `𝘾𝙄 𝙏𝙍𝘼𝙎𝙁𝙀𝙍𝙄𝘼𝙈𝙊 𝙌𝙐𝙄: ${link}`,
         mentions
