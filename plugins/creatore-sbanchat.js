ae573ea5b0c395982043bf9df95dece5390824e5
dthbot <blebledarius@gmail.com>
2026-02-16 03:35:18 +0100

    Create creatore-sbanchat.js

---
+++ plugins/creatore-sbanchat.js
@@ -0,0 +1,22 @@
+let handler = async (m) => {
+  global.db.data.chats[m.chat].isBanned = false;
+  let message = '𝐌𝐨𝐝𝐚𝐥𝐢𝐭à 𝐀𝐅𝐊 𝐝𝐢𝐬𝐚𝐭𝐭𝐢𝐯𝐚𝐭𝐚, 𝐬𝐨𝐧𝐨 𝐭𝐨𝐫𝐧𝐚𝐭𝐨 𝐑𝐈𝐊𝐊𝐈𝐎𝐍𝐈 ✓';
+  await conn.sendMessage(m.chat, { 
+      text: message,
+      contextInfo: {
+          forwardingScore: 99,
+          isForwarded: true,
+          forwardedNewsletterMessageInfo: {
+              newsletterJid: '',
+              serverMessageId: '',
+              newsletterName: '𝔻𝕋ℍ-𝔹𝕆𝕋'
+          }
+      }
+  }, { quoted: m });
+};
+
+handler.help = ['unbanchat'];
+handler.tags = ['owner'];
+handler.command = /^wakeywakey$/i;
+handler.rowner = true;
+export default handler;
