c93862331996f2c0323c56b73aed73d0e0a9ceaf
dthbot <blebledarius@gmail.com>
2026-02-16 05:40:47 +0100

    Create menu-ludopatici.js

---
+++ plugins/menu-ludopatici.js
@@ -0,0 +1,26 @@
+const handler = async (message, { conn, usedPrefix = '.' }) => {
+
+    const menuText = `
+🩸 𝐍𝚵𝑿𝐒𝐔𝐒 – *MENU LUDOPATICI* 🛡️
+
+════════════════════
+🛠️ 𝐂𝐎𝐌𝐀𝐍𝐃𝐈 *PER LUDOPATICI*
+➤ ${usedPrefix}wallet 👛
+➤ ${usedPrefix}banca 🏦
+➤ ${usedPrefix}deposita 💰
+➤ ${usedPrefix}slot 🎰
+➤ ${usedPrefix}prelievo 💰
+
+════════════════════
+🔖 Versione: *1.0*
+`.trim();
+
+    // INVIO SOLO TESTO
+    await conn.sendMessage(message.chat, { text: menuText });
+};
+
+handler.help = ['menuludopatici'];
+handler.tags = ['menu'];
+handler.command = /^(ludopatici)$/i;
+
+export default handler;
