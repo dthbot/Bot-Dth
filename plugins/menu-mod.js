8ddbc955e0000031c62ed0176bd168f263c5b39f
dthbot <blebledarius@gmail.com>
2026-03-04 17:12:56 +0100

    Update menu-mod.js

--- plugins/menu-mod.js
+++ plugins/menu-mod.js
@@ -1,20 +1,20 @@
 const handler = async (message, { conn, usedPrefix = '.' }) => {
 
     const menuText = `
-╔═══════════════╗
+╔══════════════╗
    🛡️ 𝐌𝐎𝐃 𝐏𝐀𝐍𝐄𝐋 🛡️
-╚═══════════════╝
+╚══════════════╝
 
 ━━━━━━━━━━━━━━━━━━━━
 🛠️ 𝐂𝐎𝐌𝐀𝐍𝐃𝐈 𝐌𝐎𝐃𝐄𝐑𝐀𝐓𝐎𝐑𝐈 👮
 ━━━━━━━━━━━━━━━━━━━━
 
-🧙‍♂️ ➤ ${usedPrefix}tagmod  
-⚡ ➤ ${usedPrefix}pingmod  
-🚫 ➤ ${usedPrefix}delm  
-💀 ➤ ${usedPrefix}nukegp  
-⚠️ ➤ ${usedPrefix}warnmod  
-✅ ➤ ${usedPrefix}unwarnmod  
+🧙‍♂️ ➤ ${usedPrefix}𝐭𝐚𝐠𝐦𝐨𝐝  
+⚡ ➤ ${usedPrefix}𝐩𝐢𝐧𝐠𝐦𝐨𝐝  
+🚫 ➤ ${usedPrefix}𝐝𝐞𝐥𝐦  
+💀 ➤ ${usedPrefix}𝐧𝐮𝐤𝐞𝐠𝐩  
+⚠️ ➤ ${usedPrefix}𝐰𝐚𝐫𝐧𝐦𝐨𝐝  
+✅ ➤ ${usedPrefix}𝐮𝐧𝐰𝐚𝐫𝐧𝐦𝐨𝐝  
 
 
 ━━━━━━━━━━━━━━━━━━━━
