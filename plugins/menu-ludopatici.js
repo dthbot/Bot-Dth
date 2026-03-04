4f19a309f5619ef30a600168467c9ca05defc055
dthbot <blebledarius@gmail.com>
2026-03-04 17:15:04 +0100

    Update menu-ludopatici.js

--- plugins/menu-ludopatici.js
+++ plugins/menu-ludopatici.js
@@ -1,24 +1,29 @@
 const handler = async (message, { conn, usedPrefix = '.' }) => {
 
     const menuText = `
-🩸 𝐍𝚵𝑿𝐒𝐔𝐒 – *MENU SOLDI* 🛡️
+╔════════════════╗
+    💰 𝐌𝐄𝐍𝐔 𝐒𝐎𝐋𝐃𝐈 💰
+╚════════════════╝
 
-════════════════════
-🛠️ 𝐂𝐎𝐌𝐀𝐍𝐃𝐈 *PER FARE SOLDI*
-➤ ${usedPrefix}wallet 👛
-➤ ${usedPrefix}banca 🏦
-➤ ${usedPrefix}deposita 💰
-➤ ${usedPrefix}slot 🎰
-➤ ${usedPrefix}crimine 🥷🏻
-➤ ${usedPrefix}elemosina 😅
-➤ ${usedPrefix}lavora 💼
-➤ ${usedPrefix}prelievo 💰
+━━━━━━━━━━━━━━━━━━━━
+💸 𝐂𝐎𝐌𝐀𝐍𝐃𝐈 𝐏𝐄𝐑 𝐆𝐔𝐀𝐃𝐀𝐆𝐍𝐀𝐑𝐄 💎
+━━━━━━━━━━━━━━━━━━━━
 
-════════════════════
-🔖 Versione: *1.0*
+👛 ➤ ${usedPrefix}𝐰𝐚𝐥𝐥𝐞𝐭  
+🏦 ➤ ${usedPrefix}𝐛𝐚𝐧𝐜𝐚  
+💰 ➤ ${usedPrefix}𝐝𝐞𝐩𝐨𝐬𝐢𝐭𝐚  
+🎰 ➤ ${usedPrefix}𝐬𝐥𝐨𝐭  
+🥷🏻 ➤ ${usedPrefix}𝐜𝐫𝐢𝐦𝐢𝐧𝐞  
+😅 ➤ ${usedPrefix}𝐞𝐥𝐞𝐦𝐨𝐬𝐢𝐧𝐚  
+💼 ➤ ${usedPrefix}𝐥𝐚𝐯𝐨𝐫𝐚  
+🏧 ➤ ${usedPrefix}𝐩𝐫𝐞𝐥𝐢𝐞𝐯𝐨  
+
+
+━━━━━━━━━━━━━━━━━━━━
+🔖 𝐕𝐞𝐫𝐬𝐢𝐨𝐧𝐞: 1.0 🚀
+━━━━━━━━━━━━━━━━━━━━
 `.trim();
 
-    // INVIO SOLO TESTO
     await conn.sendMessage(message.chat, { text: menuText });
 };
 
